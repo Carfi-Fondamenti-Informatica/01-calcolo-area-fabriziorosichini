a4f9fd440cef68ac8692cb84e59aae401f9b5059
fabriziorosichini <127482458+fabriziorosichini@users.noreply.github.com>
2023-03-24 16:04:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int a=0, b=0,c=0, Triangolo=0, Quadrato=0, Rettangolo=0, Trapezio=0;
+    float a=0, b=0,c=0, Triangolo=0, Quadrato=0, Rettangolo=0, Trapezio=0;
     cin >> a >> b >> c;
     Triangolo=a*b/2;
     Quadrato=a*a;
@@ -15,3 +15,4 @@ int main(){
     return 0;
 
 }
+
