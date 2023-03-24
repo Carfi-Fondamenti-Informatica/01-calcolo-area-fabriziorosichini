e384fca34bf1968655e8fe5da7a5a2cf3eaab56b
fabriziorosichini <127482458+fabriziorosichini@users.noreply.github.com>
2023-03-24 15:58:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
+    int a=0, b=0,c=0, Triangolo=0, Quadrato=0, Rettangolo=0, Trapezio=0;
+    cin >> a >> b >> c;
+    Triangolo=a*b/2;
+    Quadrato=a*a;
+    Rettangolo=a*b;
+    Trapezio=(a+b)*c/2;
+    cout<<Triangolo<<endl;
+    cout<<Quadrato<<endl;
+    cout<<Rettangolo<<endl;
+    cout<<Trapezio<<endl;
+    return 0;
 
 }
